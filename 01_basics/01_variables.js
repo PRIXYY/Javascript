13cc9e49dc0d969b06a37c8770c16d2bd9b1b9ab
Preet <preet16vaishnav@gmail.com>
2024-07-07 22:27:59 +0530

    learning basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "preet@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"  //prefer not to use this,but remember this can be done as java script is simple
+let accountState
+// accountId=2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity="Bengaluru"
+/*
+Never use var because of issue in block scope and functional scope,
+Also semicolon is not necessary in java script ,if you want you can use
+*/
+//console.log(accountId);
+console.table([accountId, accountEmail , accountPassword,accountCity,accountState ]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version of Java script
+//alert(3+3) // this only works in browser not in nodejs
+
+
+let name = "preet" // string => ""
+let age = 18  // number => 2 to power 53(range)  ,bigint also
+let Is_valid = true // boolean => true/false
+
+//null => standalone value ,there is no value
+// undefined => value has not been assigned yet,when you declare a variable without giving it a value
+//symbol = unique
+
+//object 
+
+console.log(typeof "Preet");
+console.log(typeof age)
+console.log(typeof null) //o/p = object
+console.log(typeof undefined) //output -undefined
+
+
